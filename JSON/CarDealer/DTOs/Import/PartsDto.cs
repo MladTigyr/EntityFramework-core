e9b6f52b98c24118d699756fe7f2641e7908ecee
Alex <mladtigyr@gmail.com>
2025-11-10 23:53:15 +0200

    adding some more JSON imports

---
+++ JSON/CarDealer/DTOs/Import/PartsDto.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs.Import
+{
+    public class PartsDto
+    {
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [JsonProperty("price")]
+        public string Price { get; set; } = null!;
+
+        [Required]
+        [JsonProperty("quantity")]
+        public string Quantity { get; set; } = null!;
+
+        [Required]
+        [JsonProperty("supplierId")]
+        public string SupplierId { get; set; } = null!;
+    }
+}
---
+++ JSON/CarDealer/StartUp.cs
@@ -0,0 +1,246 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new CarDealerContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+
+            string path = "../../../Datasets/";
+            string currentFile = "cars.json";
+
+            string combinedPath = Path.Combine(path, currentFile);
+
+            string inputJson = File.ReadAllText(combinedPath);
+
+            string result = ImportCars(context, inputJson);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            ICollection<Supplier> suppliers = new List<Supplier>();
+
+            IEnumerable<SuppliersDto>? suppliersDto = JsonConvert.DeserializeObject<SuppliersDto[]>(inputJson);
+
+            if (suppliersDto != null)
+            {
+                foreach (var item in suppliersDto)
+                {
+                    if (!IsValid(item))
+                    {
+                        continue;
+                    }
+
+                    bool isPropValid = bool
+                        .TryParse(item.IsImporter, out bool isImporterValue);
+
+                    if (!isPropValid)
+                    {
+                        continue;
+                    }
+
+                    Supplier supplier = new Supplier()
+                    {
+                        Name = item.Name,
+                        IsImporter = isImporterValue
+                    };
+
+                    suppliers.Add(supplier);
+                }
+
+                context.Suppliers.AddRange(suppliers);
+                context.SaveChanges();
+            }
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            ICollection<Part> parts = new List<Part>();
+
+            ICollection<int> existingSupplierIds = context.Suppliers
+                .Select(s => s.Id)
+                .ToList();
+
+            IEnumerable<PartsDto>? partsDtos = JsonConvert.DeserializeObject<PartsDto[]>(inputJson);
+
+            if (partsDtos != null)
+            {
+                foreach(PartsDto dto in partsDtos)
+                {
+                    if (!IsValid(dto))
+                    {
+                        continue;
+                    }
+
+                    bool isPriceValid = decimal
+                        .TryParse(dto.Price, out decimal priceValue);
+
+                    bool isQuantityValid = int
+                        .TryParse(dto.Quantity, out int quantityValue);
+
+                    bool isSupplierIdValid = int
+                        .TryParse(dto.SupplierId, out int supplierIdValue);
+
+                    if (!isPriceValid || !isQuantityValid || !isSupplierIdValid || !existingSupplierIds.Contains(supplierIdValue))
+                    {
+                        continue;
+                    }
+
+                    Part part = new Part()
+                    {
+                        Name = dto.Name,
+                        Price = priceValue,
+                        Quantity = quantityValue,
+                        SupplierId = supplierIdValue
+                    };
+
+                    parts.Add(part);
+                }
+
+                context.Parts.AddRange(parts);
+
+                context.SaveChanges();
+            }
+
+            return $"Successfully imported {parts.Count}.";
+        }
+
+        //public static string ImportCars(CarDealerContext context, string inputJson)
+        //{
+        //    ICollection<Car> cars = new List<Car>();
+        //    ICollection<PartCar> partsCars = new List<PartCar>();
+
+        //    ICollection<int> existingPartIds = context.Parts
+        //        .Select(p => p.Id)
+        //        .ToList();
+
+        //    IEnumerable<CarsDto>? carsDtos = JsonConvert.DeserializeObject<CarsDto[]>(inputJson);
+
+        //    if (carsDtos != null)
+        //    {
+        //        foreach(CarsDto dto in carsDtos)
+        //        {
+        //            if (!IsValid(dto))
+        //            {
+        //                continue;
+        //            }
+
+        //            bool isTraveledDistanceValid = long
+        //                .TryParse(dto.TraveledDistance, out long traveledDistanceValue);
+
+
+        //            if (!isTraveledDistanceValid)
+        //            {
+        //                continue;
+        //            }
+
+        //            Car car = new()
+        //            {
+        //                Make = dto.Make,
+        //                Model = dto.Model,
+        //                TraveledDistance = traveledDistanceValue,
+        //            };
+
+        //            cars.Add(car);
+
+        //            ICollection<int> partsIds = dto.PartsId
+        //                .Distinct()
+        //                .Where(id => existingPartIds.Contains(id))
+        //                .ToArray();
+
+        //            foreach (int partId in partsIds)
+        //            {
+        //                if (!existingPartIds.Contains(partId))
+        //                {
+        //                    continue;
+        //                }
+
+        //                PartCar currPartCar = new PartCar()
+        //                {
+        //                    Car = car,
+        //                    PartId = partId
+        //                };
+
+        //                partsCars.Add(currPartCar);
+        //            }
+        //        }
+
+        //        context.Cars.AddRange(cars);
+        //        context.PartsCars.AddRange(partsCars);
+
+        //        context.SaveChanges();
+        //    }
+
+        //    return $"Successfully imported {cars.Count}.";
+        //}
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            ICollection<Car> carsToImport = new List<Car>();
+            ICollection<PartCar> partsCarsToImport = new List<PartCar>();
+
+            IEnumerable<CarsDto>? carDtos = JsonConvert
+                .DeserializeObject<CarsDto[]>(inputJson);
+            if (carDtos != null)
+            {
+                foreach (CarsDto carDto in carDtos)
+                {
+                    if (!IsValid(carDto))
+                    {
+                        continue;
+                    }
+
+                    Car newCar = new Car()
+                    {
+                        Make = carDto.Make,
+                        Model = carDto.Model,
+                        TraveledDistance = carDto.TraveledDistance
+                    };
+                    carsToImport.Add(newCar);
+
+                    foreach (int partId in carDto.PartsId.Distinct())
+                    {
+                        if (!context.Parts.Any(p => p.Id == partId))
+                        {
+                            continue;
+                        }
+
+                        PartCar newPartCar = new PartCar()
+                        {
+                            PartId = partId,
+                            Car = newCar
+                        };
+                        partsCarsToImport.Add(newPartCar);
+                    }
+                }
+
+                //context.Cars.AddRange(carsToImport);
+                context.PartsCars.AddRange(partsCarsToImport);
+
+                context.SaveChanges();
+            }
+
+            return $"Successfully imported {carsToImport.Count}.";
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResults = new List<ValidationResult>();
+            return Validator.TryValidateObject(dto, validationContext, validationResults);
+        }
+    }
+}
